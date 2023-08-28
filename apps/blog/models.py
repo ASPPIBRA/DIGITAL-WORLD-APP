4c20466e605a5dcf39743ea1daaee8ef64e8f7ef
gustavonovais1 <gustavonovais401@gmail.com>
2023-08-28 20:57:00 -0300

    Correções de bugs

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -8,7 +8,7 @@ class BlogPost(models.Model):
     summary = models.CharField(max_length=200, verbose_name="Resumo do Post")
     text = models.TextField(verbose_name="Texto do Post")
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Usuário Criador do Post")
-    image = models.ImageField(upload_to='blog_images/', verbose_name="Imagem do Post", null=True, blank=True)
+    image = models.ImageField(upload_to='blog_images/', verbose_name="Imagem do Post")
     main_post = models.BooleanField(default=False, verbose_name="Post Principal")
     date_post = models.DateTimeField(verbose_name="Data do Post", null=True, blank=True)
 
